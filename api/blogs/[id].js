701b009abda0633f11b6068ec9e71191d833afb4
bindu <fazlerabbibindu.fintech@gmail.com>
2025-08-20 19:15:35 +0600

    url fix 06

--- api/blogs/[id].js
+++ api/blogs/[id].js
@@ -1,33 +1,26 @@
 const { connectToDatabase, collectionName } = require('../../lib/mongodb');
 const { ObjectId } = require('mongodb');
 
-module.exports = async (req, res) => {
+export default async function handler(req, res) {
   try {
     const id = (req.query.id || "").trim();
-    
+
     // Validate ID
     if (!ObjectId.isValid(id)) {
       return res.status(400).json({ error: "Invalid blog ID" });
     }
-    
+
     const db = await connectToDatabase();
     const blogId = new ObjectId(id);
-    
-    // Handle GET request
+
     if (req.method === 'GET') {
       const blog = await db.collection(collectionName).findOne({ _id: blogId });
-      
-      if (!blog) {
-        return res.status(404).json({ error: "Blog not found" });
-      }
-      
+      if (!blog) return res.status(404).json({ error: "Blog not found" });
       return res.status(200).json(blog);
     }
-    
-    // Handle PUT request
+
     if (req.method === 'PUT') {
       const { title, content, author, tags } = req.body;
-      
       const updatedBlog = {
         ...(title && { title }),
         ...(content && { content }),
@@ -35,36 +28,32 @@ module.exports = async (req, res) => {
         ...(tags && { tags }),
         updatedAt: new Date(),
       };
-      
+
       const result = await db.collection(collectionName).updateOne(
         { _id: blogId },
         { $set: updatedBlog }
       );
-      
+
       if (result.matchedCount === 0) {
         return res.status(404).json({ error: "Blog not found" });
       }
-      
+
       return res.status(200).json({ message: "Blog updated", result });
     }
-    
-    // Handle DELETE request
+
     if (req.method === 'DELETE') {
       const result = await db.collection(collectionName).deleteOne({ _id: blogId });
-      
       if (result.deletedCount === 0) {
         return res.status(404).json({ error: "Blog not found" });
       }
-      
       return res.status(200).json({ message: "Blog deleted successfully" });
     }
-    
-    // Handle other methods
+
     res.setHeader('Allow', ['GET', 'PUT', 'DELETE']);
     return res.status(405).json({ error: `Method ${req.method} not allowed` });
-    
+
   } catch (err) {
     console.error(err);
     return res.status(500).json({ error: "Server error" });
   }
-};
+}
